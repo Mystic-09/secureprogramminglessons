b91c0c4cd44cd159cf0cf99b4837513d534b2ae9
Mystic-09 <ninjawolf128@outlook.com>
2026-06-19 13:57:53 +0200

    htmlspecialchars toevoeging decription

--- transacties.php
+++ transacties.php
@@ -66,7 +66,7 @@ $incomingTransactions = $stmt->fetchAll();
             <div class="bg-red-100 p-2 rounded">
                 <?php foreach ($outgoingTransactions as $transaction): ?>
                     <div class="flex justify-between mb-2">
-                        <p><?= $transaction['description'] ?></p>
+                        <p><?= htmlspecialchars($transaction['description'], ENT_QUOTES, 'UTF-8') ?></p>
                         <p>€<?= number_format($transaction['amount'], 2, ',', '.') ?></p>
                     </div>
                 <?php endforeach; ?>
